28fd90ae74a93a4f1877bda804d4e2c96246b8e6
Arkatch <32023460+Arkatch@users.noreply.github.com>
2019-04-19 17:45:07 +0200

    v0.21

---
+++ lib8bit/bmp8gray.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+#include <stdbool.h>
+
+#ifndef __LIB8GRAY__
+#define __LIB8GRAY__
+
+#include "../maxmin.h"
+#include "binaryzation.h"
+#include "clustering.h"
+#include "convolution_matrix.h"
+#include "edgedetection.h"
+#include "filter.h"
+#include "histogram.h"
+#include "segmentation.h"
+
+#endif
